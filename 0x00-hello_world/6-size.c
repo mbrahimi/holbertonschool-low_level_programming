6cf1a8e7ea6a238746ba968e625b2f68c70214c9
mehdi <mehdi53.brahimi@gmail.com>
2021-09-30 16:25:40 +0200

    6S

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,14 +8,12 @@ int main(void)
 {
 int intType;
 float floatType;
-long longType;
-long long longType;
+double doubleType;
 char charType;
 
 printf("Size of int: %zu bytes\n", sizeof(intType));
 printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of double: %zu bytes\n", sizeof(longType));
-printf("Size of double: %zu bytes\n", sizeof(longlongsType));
+printf("Size of double: %zu bytes\n", sizeof(doubleType));
 printf("Size of char: %zu byte\n", sizeof(charType));
 return (0);
 }
