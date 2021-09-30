3d1abb4777607f33c890e1adcbbca8648aa65bea
mehdi <mehdi53.brahimi@gmail.com>
2021-09-30 16:10:05 +0200

    6S

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,18 +4,17 @@
  *
  * Return: Always 0 (Success)
  */
- #include<stdio.h>
 int main(void) 
 {
-    int intType;
-    float floatType;
-    double doubleType;
-    char charType;
+int intType;
+float floatType;
+double doubleType;
+char charType;
 
-    printf("Size of int: %zu bytes\n", sizeof(intType));
-    printf("Size of float: %zu bytes\n", sizeof(floatType));
-    printf("Size of double: %zu bytes\n", sizeof(doubleType));
-    printf("Size of char: %zu byte\n", sizeof(charType));
+printf("Size of int: %zu bytes\n", sizeof(intType));
+printf("Size of float: %zu bytes\n", sizeof(floatType));
+printf("Size of double: %zu bytes\n", sizeof(doubleType));
+printf("Size of char: %zu byte\n", sizeof(charType));
     
-    return 0;
+return 0;
 }
