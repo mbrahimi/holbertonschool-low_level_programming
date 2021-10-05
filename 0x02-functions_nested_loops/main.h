4dbfb78c700468f8a339296cefe4e1a93821c265
mehdi <mehdi53.brahimi@gmail.com>
2021-10-05 20:22:43 +0200

    p

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,4 @@
-#include "main.h"
-/**
-* main - check the code
-*
-* Return: Always 0.
-*/
-int main(void)
-{
-print_alphabet();
-return (0);
-}
+#ifndef main.h
+#define main.h
+char _putchar(char c);
+void print_alphabet(void);
