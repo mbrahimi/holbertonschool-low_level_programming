e6ee5f97e6669d0b028fe0cf8251fb4f660f7c3c
mehdi <mehdi53.brahimi@gmail.com>
2021-10-05 20:21:21 +0200

    kokok

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,12 +1,5 @@
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-char c;
-for (c = 'a'; c <= 'z'; ++c)
-printf("%c ", c);
-return (0);
+#ifndef main.h
+#define main.h
+char _putchar(char c);
+void print_alphabet(void);
+    
