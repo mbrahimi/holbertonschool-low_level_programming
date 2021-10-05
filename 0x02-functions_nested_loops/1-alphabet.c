8896a4a342e0f18c9cdd9e081e236b66366e1d0d
mehdi <mehdi53.brahimi@gmail.com>
2021-10-05 19:28:25 +0200

    boon

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
 /**
 * print_alphabet - a function that prints the alphabet, in lowercase
 * Return: Always 0 (Succes)
-*/
+*/  
 void print_alphabet(void)
 {
 int n;
@@ -13,4 +13,4 @@ for (n = 'a'; n <= 'z'; n++)
 _putchar(n);
 }
 _putchar('\n');
-}
+}   
