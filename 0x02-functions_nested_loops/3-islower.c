2739d52f1714e5460da98e3f496840b4de3f121f
mehdi <mehdi53.brahimi@gmail.com>
2021-10-06 13:33:03 +0200

    ll

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
 * islower - check if a character is lower than an other one
-*
+*@c: integer value
 * Return: Always 0 (Succes)
 */
 int _islower(int c);
-if(_islower c);
+if(_islower c));
 return (1);
 else
 return (0);
