581df2b7994b6e00165de87d560513a3b8196054
mehdi <mehdi53.brahimi@gmail.com>
2021-10-06 09:49:10 +0200

    1AC

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* print_alphabet -a function that prints the alphabet, in lowercase
+* print_alphabet - a function that prints the alphabet, in lowercase
 *
 * Return: Always 0 (Succes)
 */
