d1945387a17e01e68c13494654850c75ff5b41d9
mehdi <mehdi53.brahimi@gmail.com>
2021-10-07 14:36:32 +0200

    fd

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
 * print_numbers - function write the numbers from 0 to 9
 *
@@ -10,8 +9,6 @@ void print_numbers(void)
 {
 int i;
 for (i = 0; i <= 9; i++)
-{ 
 _putchar(i + '0');
-}
- _putchar('\n');
+_putchar('\n');
 }
