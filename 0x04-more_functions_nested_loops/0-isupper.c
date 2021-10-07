dd2fb98bf1a52dac2a79d14481c70a139e1d1f93
mehdi <mehdi53.brahimi@gmail.com>
2021-10-07 10:51:57 +0200

    rr

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+/**
+* _isupper - is _isupper
+*@c: integer value
+* Return: 0 or 1
+*/
+int _isupper (int c)
+{
+if (_isupper(c))
+return (1);
+else
+return (0);
+}
