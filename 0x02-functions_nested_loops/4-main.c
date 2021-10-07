02d158191b348a3508505a752f113c49ed9ff14c
mehdi <mehdi53.brahimi@gmail.com>
2021-10-07 09:17:51 +0200

    r

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,12 +1,25 @@
 #include "main.h"
 
 /**
-* _isalpha - a function that checks for alphabetic character
-*
-* @c: single letter input
-*
-* Return: 1 if c is a letter (lower or uppercase), 0 otherwise
-*/
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = _isalpha('H');
+    _putchar(r + '0');
+    r = _isalpha('o');
+    _putchar(r + '0');
+    r = _isalpha(108);
+    _putchar(r + '0');
+    r = _isalpha(';');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
 int main(void)
 {
     int r;
