58220c1be97c12fcdd9ba191d44a1c34e8633831
mehdi <mehdi53.brahimi@gmail.com>
2021-10-07 14:15:03 +0200

    f

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,13 @@
-#include "main.h"
-#include <stdio.h>
 /**
-* void print_numbers(void); - it's void
-*
-* _putchar - for 0-9
-*/
+ * print_numbers - function for white the numbers from 0 to 9
+ *
+ * Return: 0-9 .
+ */
+void print_numbers(void)
 {
-char low;
- for (low = '0'; low <= '9'; low++);
-putchar(low);
-putchar('\n');
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
+	_putchar('\n');
 }
