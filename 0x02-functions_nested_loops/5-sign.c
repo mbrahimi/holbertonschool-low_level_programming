067a88298792ebde97a50da02d85a02bc62c1a17
mehdi <mehdi53.brahimi@gmail.com>
2021-10-07 09:41:51 +0200

    kk

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_sign (int n) - check the code 
+ *
+ * Return: can change when n is differant 
+ */
+ int print_sign(int n);
+ if ( n > 0)
+ return (1)
+ if ( n = 0 )
+ return (0)
+ if ( n < 0 )
+return (-1)
