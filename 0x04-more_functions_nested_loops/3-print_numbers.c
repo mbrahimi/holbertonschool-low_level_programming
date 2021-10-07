5ef7744b91a013fc5cd3e441a057b56e86aca7ea
mehdi <mehdi53.brahimi@gmail.com>
2021-10-07 13:41:36 +0200

    kk

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*void print_numbers(void); - it's void
+*_putchar - for 0-9
+*/
+{
+char low;
+for (low = '0'; low <= '9'; low++)
+putchar(low);
+putchar('\n');
+}
